0f60159ea8d7ecbe3d97ce0346d3e65a0d05d8c9
BryanMoragas <bryanmoragas@ceitartessos.com>
2019-10-25 17:18:56 +0200

    Ejercicio operadores

---
+++ Unity/Assets/Script/EjercicioOperadores.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjercicioOperadores : MonoBehaviour {
+
+	public int numeroUno = 1;	
+	public int numeroDos = 2;
+	public int numeroTres;
+	public float numeroCuatro = 4;
+	public float numeroCinco;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log (numeroUno + numeroTres + numeroCuatro);
+		Debug.Log (numeroUno - numeroDos - numeroCinco);
+		Debug.Log ((numeroUno + numeroDos) * numeroCinco);
+		Debug.Log ((numeroUno - numeroTres) / (numeroCinco + numeroCuatro));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
